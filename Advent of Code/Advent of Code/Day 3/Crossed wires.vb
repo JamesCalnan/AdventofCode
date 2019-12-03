53f4cea0958c39561dfc3b1b22d0ce681b0ef5cf
James Calnan <James.Calnan@outlook.com>
2019-12-03 16:37:28 +0000

    Added day 3 part 2

--- Advent of Code/Advent of Code/Day 3/Crossed wires.vb
+++ Advent of Code/Advent of Code/Day 3/Crossed wires.vb
@@ -4,7 +4,7 @@ Imports System.Drawing
 Imports System.Linq
 Module Crossed_wires
 
-    Function parseWires(filePath As String) As List(Of Point)
+    Function getPoints(filePath As String) As List(Of Point)
         Dim directions() = My.Computer.FileSystem.ReadAllText(filePath).Split(",")
         Dim newWire As New List(Of Point) From {
             New Point(0, 0)
@@ -34,18 +34,24 @@ Module Crossed_wires
     End Function
 
     Function part1()
-        Dim wire1 = parseWires("S:\AoC\Advent of Code\Advent of Code\Day 3\data1.txt")
-        Dim wire2 = parseWires("S:\AoC\Advent of Code\Advent of Code\Day 3\data2.txt")
+        Dim wire1 = getPoints("H:\AoC\Advent of Code\Advent of Code\Day 3\data1.txt")
+        Dim wire2 = getPoints("H:\AoC\Advent of Code\Advent of Code\Day 3\data2.txt")
         Dim intersections = wire1.Intersect(wire2).Where(Function(p) p.X <> 0 And p.Y <> 0)
-        Dim closest = intersections.OrderBy(Function(p) Math.Abs(p.X) + Math.Abs(p.Y)).First()
+
+        Dim enumerable As IEnumerable(Of Point) = intersections.ToList()
+        For Each wire In enumerable
+            Console.WriteLine(Math.Abs(wire.X) + Math.Abs(wire.Y))
+        Next
+
+        Dim closest = enumerable.OrderBy(Function(p) Math.Abs(p.X) + Math.Abs(p.Y)).First()
+
         Dim distance = Math.Abs(closest.X) + Math.Abs(closest.Y)
+
+
+
         Return distance
     End Function
-    Function part2()
-        Dim wire1 = parseWires("S:\AoC\Advent of Code\Advent of Code\Day 3\data1.txt")
-        Dim wire2 = parseWires("S:\AoC\Advent of Code\Advent of Code\Day 3\data2.txt")
-        Dim intersections = wire1.Intersect(wire2).Where(Function(p) p.X <> 0 And p.Y <> 0)
-        Dim closestMovement = intersections.Select(Function(p) wire1.IndexOf(p) + wire2.IndexOf(p)).OrderBy(Function(j) True).First()
-        Return closestMovement
+    Function calculateManhattenDistance(inputPoint As point)
+        Return Math.Abs(inputPoint.X) + Math.Abs(inputPoint.Y)
     End Function
 End Module
